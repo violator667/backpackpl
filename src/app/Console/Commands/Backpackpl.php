1d7272ddfb77980a315a0b95b788e508f03530ba
reseller <reseller@posejdon.g0f.pl>
2018-10-21 12:55:52 +0200

    first commit

---
+++ src/app/Console/Commands/Backpackpl.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Backpackpl extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'backpackpl:addpolish';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Ads polish translation to Laravel Backpack';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->line('Yes it runs.');
+        $from_dir = 'vendor/violator667/backpackpl/src';
+        $base = base_path();
+        $the_dir = $base.'/'.$from_dir;
+        $to_dir = $base.'/';
+        $this->line($the_dir);
+        $this->line($to_dir);
+        \File::copyDirectory($the_dir, $to_dir);
+        $this->line("Is it done?!");
+    }
+}
